063a5e6182d7cd5c092d3fe55eb5b07488f46b4e
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-05-29 15:17:13 +0100

    add an ssb server process

---
+++ config.js
@@ -0,0 +1,46 @@
+const nest = require('depnest')
+const Config = require('ssb-config/inject')
+
+const { merge } = require('lodash')
+const { join } = require('path')
+
+exports.gives = nest('config.sync.load')
+exports.create = (api) => {
+  var config
+  return nest('config.sync.load', () => {
+    if (config) return config
+
+    console.log('LOADING config')
+    config = Config(process.env.ssb_appname || 'ssb')
+
+    config = addSockets(config)
+    config = fixLocalhost(config)
+
+    return config
+  })
+}
+
+function addSockets (config) {
+  if (process.platform === 'win32') return config
+
+  const pubkey = config.keys.id.slice(1).replace(`.${config.keys.curve}`, '')
+  return merge(
+    config,
+    {
+      connections: {
+        incoming: { unix: [{ scope: 'local', transform: 'noauth', server: true }] }
+      },
+      remote: `unix:${join(config.path, 'socket')}:~noauth:${pubkey}` // overwrites
+    }
+  )
+}
+
+function fixLocalhost (config) {
+  if (process.platform !== 'win32') return config
+
+  // without this host defaults to :: which doesn't work on windows 10?
+  config.connections.incoming.net[0].host = '127.0.0.1'
+  config.connections.incoming.ws[0].host = '127.0.0.1'
+  config.host = '127.0.0.1'
+  return config
+}
