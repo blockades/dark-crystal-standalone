be487eb205ed8d66aa30e05b18393ea6a24c3738
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-06-05 19:12:32 +0100

    define routes fn's for easy accessibility

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+// Define route functions here, then in order to use them, add to router/async/routes.js with the relevant view
+const RootPath = (request) => request.path === `/`
+
+const SecretsIndexPath = (request) => request.path === `/secrets`
+const SecretsShowPath = (request) => request.secret && request.path === `/secrets/${request.secret.id}`
+
+// Saved state between different views for a form - gives better customisability
+const SecretsNewPath = (request) => request.path === `/secrets/new`
+const SecretsNewNamePath = (request) => request.path === `/secrets/new/name`
+const SecretsNewSecretPath = (request) => request.state && request.state.secretName && request.path === `/secrets/new/secret`
+const SecretsNewTrustPath = (request) => request.state && request.state.secretName && request.state.secret && request.path === `/secrets/new/trust`
+const SecretsNewSubmitPath = (request) => (
+  request.state && request.state.secretName && request.state.secret &&
+    request.state.quorum && request.state.peers && request.path === `/secrets/new/submit`
+)
+
+const SettingsIndexPath = (request) => request.path === `/settings`
+const SettingsAccountIndexPath = (request) => request.path === `/settings/account`
+const SettingsNetworkIndexPath = (request) => request.path === `/settings/network`
+
+const isSecretsNamespace = (request) => request.path.substring(0, 8) === `/secrets`
+const isSettingsNamespace = (request) => request.path.substring(0, 9) === `/settings`
+
+module.exports = {
+  RootPath,
+
+  SecretsIndexPath,
+  SecretsShowPath,
+  SecretsNewPath,
+
+  SecretsNewNamePath,
+  SecretsNewSecretPath,
+  SecretsNewTrustPath,
+  SecretsNewSubmitPath,
+
+  SettingsIndexPath,
+  SettingsAccountIndexPath,
+  SettingsNetworkIndexPath,
+
+  isSettingsNamespace,
+  isSecretsNamespace,
+}
